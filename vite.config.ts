cd34998e47f26f18e46b4e5d4e48704812985313
EducRodTablizo <rodbenedictbtablizo@iskolarngbayan.edu.ph>
2026-06-03 14:29:36 +0800

    completed recovery email password reset

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { enterDevPlugin, enterProdPlugin } from 'vite-plugin-enter-dev';
 
 export default defineConfig({
-  plugins: [...enterProdPlugin(), ...enterDevPlugin(), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
