bdf46cf585cb4318ccef7d2074670851009a1dd9
enter-pro <github@enter.pro>
2026-06-03 06:30:23 +0000

    Add vite-plugin-enter-dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { enterDevPlugin, enterProdPlugin } from 'vite-plugin-enter-dev';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [...enterProdPlugin(), ...enterDevPlugin(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
